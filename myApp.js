62c0128d14639005487bcc9b8f0a9d1ddcb8d2e0
millymats2 <97088594+millymats2@users.noreply.github.com>
2023-05-26 11:19:24 -0700

    dir

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get( '/', (req, res) => {
-    res.send(absolutePath = __dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 
